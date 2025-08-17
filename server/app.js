ebf11ba4486a6a48e041607d64144e1d08164f50
hisekr <abhishekroshan11@gmail.com>
2025-08-17 15:16:48 +0530

    frontend cors url update

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://youtube-companion-dashboard-client.vercel.app/",
     credentials: true,
   })
 );
